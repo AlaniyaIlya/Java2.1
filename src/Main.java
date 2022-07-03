6a3d37c0063461288deeae6f9de82dde51974bbd
Алания Илья <alaniyailya@mail.ru>
2022-07-03 22:29:44 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float price = 50000;
+        int priceAir = 20;
+        int miles = (int)  price / priceAir;
+        System.out.println(miles);
+    }
+}
